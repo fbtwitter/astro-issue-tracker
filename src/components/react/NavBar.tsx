c9b5cf02a38263d58777f1c4e2050339821785c0
Reza Fauzi Augusdi <rzaugusdi354@gmail.com>
2024-02-29 20:47:08 +0700

    setup radix ui

--- src/components/react/NavBar.tsx
+++ src/components/react/NavBar.tsx
@@ -18,16 +18,16 @@ const NavBar = ({ astroAPI }: { astroAPI: APIContext }) => {
   const { url } = astroAPI
 
   return (
-    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
-      <a href="/public"><AiFillBug/></a>
-      <ul className="flex space-x-6">
-        {links.map((link) =>
-          <a key={link.href} href={link.href}
-             className={clsx(link.href === url.pathname ? 'text-red-900' : 'text-zinc-500', 'hover:text-zinc-800' +
-               ' transition-colors')}>{link.label}
-          </a>)}
-      </ul>
-    </nav>
+      <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+        <a href="/public"><AiFillBug/></a>
+        <ul className="flex space-x-6">
+          {links.map((link) =>
+            <a key={link.href} href={link.href}
+               className={clsx(link.href === url.pathname ? 'text-red-900' : 'text-zinc-500', 'hover:text-zinc-800' +
+                 ' transition-colors')}>{link.label}
+            </a>)}
+        </ul>
+      </nav>
   )
 }
 
